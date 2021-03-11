89440a418fbc9b3b9c8648a987bb8101ffd716a9
Carolina Menezes <60782333+carolinamenezes@users.noreply.github.com>
2021-03-11 14:51:05 -0300

    Release v0.1.0
    * Update logs.ts
    
    * Automatic update on Toolbelt plugin documentation
    
    * Remove color script
    
    * CHANGELOG
    
    * Automatic update on Toolbelt plugin documentation
    
    * Release v0.1.0
    
    * Automatic update on Toolbelt plugin documentation
    
    Co-authored-by: Thiago Veras <thiago.veras.machado@gmail.com>

--- src/commands/logs.ts
+++ src/commands/logs.ts
@@ -1,24 +1,33 @@
 import { flags as oclifFlags } from '@oclif/command'
 
-import { CustomCommand } from 'vtex'
+import { CustomCommand, ColorifyConstants } from 'vtex'
 import appsLogs from '../modules/logs'
 
 export default class Logs extends CustomCommand {
-  static description = 'Show apps production logs'
+  static description = 'Shows logs of an app. (Only apps in production.)'
 
-  static examples = ['vtex logs', 'vtex logs appName', 'vtex logs --all', 'vtex logs appName --past']
+  static examples = [
+    `${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex logs')}`,
+    `${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex logs')} appName`,
+    `${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex logs')} --all`,
+    `${ColorifyConstants.COMMAND_OR_VTEX_REF('vtex logs')} appName --past`,
+  ]
 
   static flags = {
     ...CustomCommand.globalFlags,
-    all: oclifFlags.boolean({ char: 'a', description: "Show all logs from this account's apps", default: false }),
+    all: oclifFlags.boolean({
+      char: 'a',
+      description: `Shows logs of every app installed on the current ${ColorifyConstants.ID('account')}.`,
+      default: false,
+    }),
     past: oclifFlags.boolean({
       char: 'p',
-      description: "Show logs already seen from this account's apps",
+      description: `Shows previous logs of the specified app.`,
       default: false,
     }),
   }
 
-  static args = [{ name: 'app', required: false }]
+  static args = [{ name: 'app', required: false, description: `Name of the app to show logs.` }]
 
   async run() {
     const {
